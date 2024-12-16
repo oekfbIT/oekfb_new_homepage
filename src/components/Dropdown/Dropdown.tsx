fc51942a6339fb8875c3b1be7e30b177562f2095
Alon Yakoby <alon.yakoby@gmail.com>
2024-12-16 20:59:26 +0100

    dropdown test

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,66 +1,102 @@
-/*
-We're constantly improving the code you see. 
-Please share your feedback here: https://form.asana.com/?k=uvp-HPgd3_hyoXRBw1IcNg&d=1152665201300829
-*/
-
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
 interface Props {
-  className: any;
-  dropdownWrapperClassName: any;
-  matLabelSelectAClassName: any;
-  text: string;
-  dropdownWrapperClassNameOverride: any;
-  text1: string;
-  dropdownWrapper: string;
+  className?: string;
+  dropdownWrapperClassName?: string;
+  matLabelSelectAClassName?: string;
+  text?: string;
+  dropdownWrapperClassNameOverride?: string;
+  placeholder?: string;
+  options: any[]; // Dropdown options data
+  displayKey: string; // Key for displayed text
+  valueKey: string; // Key for the returned value
+  onChange: (value: string) => void; // Callback for value changes
 }
 
 export const Dropdown = ({
-  className,
-  dropdownWrapperClassName,
-  matLabelSelectAClassName,
-  text = "Select a region",
-  dropdownWrapperClassNameOverride,
-  text1 = "Wien",
-  dropdownWrapper = "/img/dropdown-wrapper-icon.svg",
-}: Props): JSX.Element => {
+                           className,
+                           dropdownWrapperClassName,
+                           matLabelSelectAClassName,
+                           text = "Select a region",
+                           dropdownWrapperClassNameOverride,
+                           placeholder = "Choose an option",
+                           options = [],
+                           displayKey,
+                           valueKey,
+                           onChange,
+                         }: Props): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false); // Toggle dropdown
+  const [selectedValue, setSelectedValue] = useState<string | null>(null);
+
+  const toggleDropdown = () => setIsOpen((prev) => !prev);
+
+  const handleSelect = (value: string) => {
+    setSelectedValue(value);
+    setIsOpen(false);
+    onChange(value);
+  };
+
   return (
-    <div className={`dropdown ${className}`}>
-      <div className="dropdown-wrapper">
-        <div className="dropdown-wrapper-2">
-          <div
-            className={`mat-label-select-a-wrapper ${dropdownWrapperClassName}`}
-          >
-            <div className={`mat-label-select-a ${matLabelSelectAClassName}`}>
-              {text}
-            </div>
-          </div>
-
-          <div className="dropdown-wrapper-3">
+      <div className={`dropdown ${className}`}>
+        <div className="dropdown-wrapper">
+          <div className="dropdown-wrapper-2">
             <div
-              className={`dropdown-wrapper-4 ${dropdownWrapperClassNameOverride}`}
+                className={`mat-label-select-a-wrapper ${dropdownWrapperClassName}`}
             >
-              {text1}
+              <div className={`mat-label-select-a ${matLabelSelectAClassName}`}>
+                {text}
+              </div>
             </div>
 
-            <img
-              className="dropdown-wrapper-5"
-              alt="Dropdown wrapper"
-              src={dropdownWrapper}
-            />
+            {/* Trigger with existing styling */}
+            <div className="dropdown-wrapper-3" onClick={toggleDropdown}>
+              <div
+                  className={`dropdown-wrapper-4 ${dropdownWrapperClassNameOverride}`}
+              >
+                {selectedValue
+                    ? options.find((opt) => opt[valueKey] === selectedValue)?.[displayKey]
+                    : placeholder}
+              </div>
+              <img
+                  className="dropdown-wrapper-5"
+                  alt="Dropdown wrapper"
+                  src="/img/dropdown-wrapper-icon.svg"
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="dropdown-wrapper-6" />
-    </div>
+        {/* Dropdown options list */}
+        {isOpen && (
+            <div className="dropdown-wrapper-6">
+              <ul className="dropdown-options">
+                {options.map((option) => (
+                    <li
+                        key={option.id || option[valueKey]}
+                        onClick={() => handleSelect(option[valueKey])}
+                        className="dropdown-option-item"
+                    >
+                      {option[displayKey]}
+                    </li>
+                ))}
+              </ul>
+            </div>
+        )}
+      </div>
   );
 };
 
 Dropdown.propTypes = {
   text: PropTypes.string,
-  text1: PropTypes.string,
-  dropdownWrapper: PropTypes.string,
+  placeholder: PropTypes.string,
+  options: PropTypes.array.isRequired,
+  displayKey: PropTypes.string.isRequired,
+  valueKey: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+Dropdown.defaultProps = {
+  options: [],
 };
