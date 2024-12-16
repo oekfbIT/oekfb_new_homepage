f12898f00d7e0af39251fd2c73e202ec25acd7af
Alon Yakoby <alon.yakoby@gmail.com>
2024-12-16 21:18:54 +0100

    dropdown test

--- src/network/ApiService.js
+++ src/network/ApiService.js
@@ -1,7 +1,7 @@
 class ApiService {
     constructor() {
         // this.baseURL = "https://api.oekfb.eu";
-        this.baseURL = "http://localhost:8080";
+        this.baseURL = "http://localhost:8080/webClient";
     }
 
     async request(method, endpoint, body = null, headers = {}) {
--- src/screens/ElementLeagueSelection/ElementLeagueSelection.tsx
+++ src/screens/ElementLeagueSelection/ElementLeagueSelection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useWindowWidth } from "../../breakpoints";
 import { Dropdown } from "../../components/Dropdown";
 import { Footer } from "../../components/Footer";
@@ -7,28 +7,39 @@ import { Sponsors } from "../../components/Sponsors";
 import { ViewDefaultWrapper } from "../../components/ViewDefaultWrapper";
 import "./style.css";
 import { Navigation } from "../../components/Navigation";
+import ClientController from "../../network/ClientController";
 
 export const ElementLeagueSelection = (): JSX.Element => {
     const screenWidth = useWindowWidth();
+    const clientController = new ClientController();
 
-    const leagues = [
-        {
-            state: "wien",
-            teamcount: 12,
-            name: "1. Wiener Liga",
-            id: "B236BAAD-404C-4451-9C30-122CBD9EB0DA",
-            code: "W1",
-        },
-        {
-            state: "wien",
-            teamcount: 14,
-            name: "2. Wiener Liga A",
-            id: "80B164B6-4015-4D95-A432-D85CBAD5AF8B",
-            code: "W2A",
-        },
-    ];
+    // State to store league options and selected value
+    const [leagues, setLeagues] = useState<any[]>([]);
+    const [selectedLeague, setSelectedLeague] = useState<string>("W1"); // Default selection "W1"
 
+    // Fetch league options
+    useEffect(() => {
+        const fetchLeagues = async () => {
+            try {
+                const data = await clientController.fetchLeagueSelection();
+
+                // Filter out unwanted leagues (name = "Mannschaft aus der Liga ausgetreten")
+                const filteredLeagues = data.filter(
+                    (league) => league.name !== "Mannschaft aus der Liga ausgetreten"
+                );
+
+                setLeagues(filteredLeagues);
+            } catch (error) {
+                console.error("Error fetching leagues:", error);
+            }
+        };
+
+        fetchLeagues();
+    }, []);
+
+    // Handle dropdown selection change
     const handleDropdownChange = (selectedCode: string) => {
+        setSelectedLeague(selectedCode);
         console.log("Selected League Code:", selectedCode);
     };
 
@@ -36,12 +47,7 @@ export const ElementLeagueSelection = (): JSX.Element => {
         <div
             className="element-league-selection"
             style={{
-                minWidth:
-                    screenWidth < 900
-                        ? "390px"
-                        : screenWidth >= 900
-                            ? "900px"
-                            : undefined,
+                minWidth: screenWidth < 900 ? "390px" : screenWidth >= 900 ? "900px" : undefined,
             }}
         >
             {screenWidth < 900 && (
@@ -52,9 +58,9 @@ export const ElementLeagueSelection = (): JSX.Element => {
                             <div className="page-header-wrapper-2">
                                 <div className="page-header-wrapper-3">TITLE</div>
                                 <p className="page-header-wrapper-4">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
-                                    nec varius lorem, eget faucibus ex. Vivamus vehicula dui non
-                                    leo laoreet luctus. Etiam congue consequat placerat.
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras nec varius lorem,
+                                    eget faucibus ex. Vivamus vehicula dui non leo laoreet luctus. Etiam congue
+                                    consequat placerat.
                                 </p>
                             </div>
                         </div>
@@ -77,10 +83,7 @@ export const ElementLeagueSelection = (): JSX.Element => {
                             </div>
                         </div>
 
-                        <Sponsors
-                            className="instance-node-2"
-                            vWhite="/img/v-white-1-1.svg"
-                        />
+                        <Sponsors className="instance-node-2" vWhite="/img/v-white-1-1.svg" />
                     </div>
 
                     <Footer
@@ -105,42 +108,8 @@ export const ElementLeagueSelection = (): JSX.Element => {
                         className="instance-node"
                         img="/img/league-row-item-content-seperator-1080.svg"
                         leagueRowItem="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem1="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem10="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem11="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem12="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem13="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem14="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem15="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem16="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem2="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem3="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem4="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem5="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem6="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem7="/img/league-row-item-content-img-1090.png"
-                        leagueRowItem8="/img/league-row-item-content-seperator-1080.svg"
-                        leagueRowItem9="/img/league-row-item-content-img-1090.png"
-                        mobileBurgerMenu="/img/mobile-burger-menu-13.svg"
-                        navRowWrapper="/img/nav-row-wrapper-content-logo-13.svg"
-                        navRowWrapper1="/img/nav-row-wrapper-content-login-wrapper-image-6.svg"
                         view="default"
                     />
-                    <div className="div-14">
-                        <div className="page-header-wrapper">
-                            <div className="page-header-3">
-                                <div className="page-header-wrapper-2">
-                                    <div className="page-header-wrapper-3">TITLE</div>
-                                    <p className="page-header-wrapper-4">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                        Cras nec varius lorem, eget faucibus ex. Vivamus vehicula
-                                        dui non leo laoreet luctus. Etiam congue consequat placerat.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                     <div className="page-content-2">
                         <div className="leagues-wrapper-2">
                             <Dropdown
@@ -158,13 +127,7 @@ export const ElementLeagueSelection = (): JSX.Element => {
                             </div>
                         </div>
 
-                        <div className="div-14">
-                            <Sponsors
-                                className="sponsors-3"
-                                sponsorsContainerClassName="sponsors-instance"
-                                vWhite="/img/v-white-1-2.svg"
-                            />
-                        </div>
+                        <Sponsors className="sponsors-3" vWhite="/img/v-white-1-2.svg" />
                     </div>
 
                     <Footer
