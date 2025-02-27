a9def18e866e9f2b098ca9b7d78a92245a5fab26
Alon Yakoby <alon.yakoby@gmail.com>
2025-02-27 20:47:07 +0100

    api service

--- src/network/ApiService.js
+++ src/network/ApiService.js
@@ -1,7 +1,7 @@
 class ApiService {
   constructor() {
-    this.baseURL = "https://test.oekfb.eu";
-    // this.baseURL = "https://api.oekfb.eu";
+    // this.baseURL = "https://test.oekfb.eu";
+    this.baseURL = "https://api.oekfb.eu";
     // this.baseURL = "http://localhost:8080";
   }
 
