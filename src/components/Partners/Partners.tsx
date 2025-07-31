b36e958212e326896ccafadec919373407f4d809
Alon Yakoby <alon.yakoby@gmail.com>
2025-07-31 05:58:03 +0200

    logos partners

--- src/components/Partners/Partners.tsx
+++ src/components/Partners/Partners.tsx
@@ -1,5 +1,4 @@
 import PropTypes from "prop-types";
-import React from "react";
 import "./style.css";
 
 interface Props {
@@ -10,7 +9,7 @@ interface Props {
 
 export const Partners = ({
                            className,
-                           vWhite = "/img/v-white-1.svg",
+                           vWhite = "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fpartner%2Fv-white-1.svg?alt=media&token=68497c24-7f68-4b7c-887b-1c083c1574dd",
                            sponsorsContainerClassName,
                          }: Props): JSX.Element => {
   return (
@@ -27,20 +26,20 @@ export const Partners = ({
               <img
                   className="sportsselection"
                   alt="Sportsselection"
-                  src="/img/sportsselection-2zlg-subl-g-t-1.png"
+                  src="https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fpartner%2Fsportsselection-2zlg-subl-g-t-1.png?alt=media&token=eb2816ab-9b72-43ca-8a95-8aef1282ddbf"
               />
             </a>
 
             <a href="https://www.westsidesoccer.at/" target="_blank" rel="noopener noreferrer">
-              <img className="image-2" alt="Image" src="/img/image-1-1.png" />
+              <img className="image-2" alt="Image" src="https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fpartner%2Fimage-1-1.png?alt=media&token=aaa1f15a-378e-40aa-990c-497d950b9b5d" />
             </a>
 
             <a href="https://www.veo.co/de" target="_blank" rel="noopener noreferrer">
-              <img className="image-3" alt="Image" src="/img/image-2-1.png" />
+              <img className="image-3" alt="Image" src="https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fpartner%2Fimage-2-1.png?alt=media&token=93cda297-1700-4e81-b782-99f746ec29a4" />
             </a>
 
             <a href="https://www.minifootball.com/" target="_blank" rel="noopener noreferrer">
-              <img className="image-4" alt="Image" src="/img/wmfLogo.png" />
+              <img className="image-4" alt="Image" src="https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fpartner%2FwmfLogo.png?alt=media&token=1508d0ab-bfc8-4126-aebc-10c43acf2b48" />
             </a>
 
             <a href="https://www.minifootball.eu/" target="_blank" rel="noopener noreferrer">
