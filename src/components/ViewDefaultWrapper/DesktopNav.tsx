fcc84bd392ae79fd6379ae662977a1da05fbc4ce
Alon Yakoby <alon.yakoby@gmail.com>
2025-07-31 05:49:33 +0200

    updated online nav links

--- src/components/ViewDefaultWrapper/DesktopNav.tsx
+++ src/components/ViewDefaultWrapper/DesktopNav.tsx
@@ -19,79 +19,79 @@ export const DesktopNav = ({
   // League Rows Data
   const leagueRows = [
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "1. Wiener Liga",
       code: "W1",
       id: "B236BAAD-404C-4451-9C30-122CBD9EB0DA",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "2. Wiener Liga A",
       code: "W2A",
       id: "80B164B6-4015-4D95-A432-D85CBAD5AF8B",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "2. Wiener Liga B",
       code: "W2B",
       id: "F6813EF5-BF80-4A2D-B9B3-3C5845A9CC98",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "3. Wiener Liga A",
       code: "W3A",
       id: "69A15306-4F1E-4A5B-AE64-5487B7B99CDD",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "3. Wiener Liga B",
       code: "W3B",
       id: "77F04A92-7E91-4C65-9E96-0FA1E1033805",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "4. Wiener Liga A",
       code: "W4A",
       id: "F58269FD-DE67-4E5D-BBD2-780877555F2B",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "4. Wiener Liga B",
       code: "W4B",
       id: "29FFA358-A32B-4F60-904B-D66B118E1661",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "1. Liga Linz",
       code: "L1",
       id: "D506E56C-3877-4581-90F0-26D144EFBE83",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "1. Liga Graz",
       code: "G1",
       id: "E171419D-C82E-4620-B691-B0D7BFEFB154",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "1. Liga Salzburg",
       code: "SBZ",
       id: "4C0521AB-B28B-4C5B-A147-E6BA50042348",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "1. Liga Innsbruck",
       code: "IBK",
       id: "AD2C1309-7F78-4E31-9FFC-957D1A38A43E",
     },
     {
-      img: "/img/league-row-item-content-img-90.png",
+      img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",
       text: "2. Liga Innsbruck",
       code: "IBK2",
       id: "554ACBEE-A0D3-4E5E-8DCF-50825136C3C8",
     },
-    {img: "/img/league-row-item-content-img-90.png",text: "1. Liga Niederösterreich", code: "NÖ1", id: "B844ED89-6766-49BC-9996-5572AA7B1FB7" }, // new
-    { img: "/img/league-row-item-content-img-90.png", text: "1. Liga Bregenz", code: "BGZ", id: "32B5B0E6-7F8E-4F82-8837-F6A9EE103D91" }, // new
+    {img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc",text: "1. Liga Niederösterreich", code: "NÖ1", id: "B844ED89-6766-49BC-9996-5572AA7B1FB7" }, // new
+    { img: "https://firebasestorage.googleapis.com/v0/b/oekfbbucket.appspot.com/o/adminfiles%2Fhomepage%2Fleague-row-item-content.png?alt=media&token=78fbe411-ed2f-4779-947c-6390725f56dc", text: "1. Liga Bregenz", code: "BGZ", id: "32B5B0E6-7F8E-4F82-8837-F6A9EE103D91" }, // new
   ];
 
   // Fetch active league from cookie on mount
